5487788e337226d4b96c3f1921865ed7fb2b0ed1
Qwesh157 <55902315+Qwesh157@users.noreply.github.com>
2024-01-09 18:59:14 +0800

    Update convolution layer definition

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # PyTorch Custom Convolution Layer
 This project is about how to define a custom convolution layer in PyTorch, and use CUDA function to implement convolution.
 
+## Content
+
+[/cpp](https://github.com/Qwesh157/pytorch_custom_convolution_layer/blob/main/cpp) C++ extension include CUDA interface and Python module bind.
+[/cuda](https://github.com/Qwesh157/pytorch_custom_convolution_layer/tree/main/cuda) Implicit gemm convolution implementation.
+[/include](https://github.com/Qwesh157/pytorch_custom_convolution_layer/tree/main/include) Declaration about forward/backward convolution.  
+[/pytorch](https://github.com/Qwesh157/pytorch_custom_convolution_layer/tree/main/pytorch) Include setup.py script, custom convolution layer definition.  
+
+
 ## Build
 
 ```bash
@@ -8,3 +16,7 @@ $ sh setup.sh
 ```
 
 If you don't have root permission, add environment option `--prefix="/home/user/.conda/envs/yourenvname/`.
+
+## Run
+
+
---
+++ pytorch/conv_layer.py
@@ -0,0 +1,56 @@
+import conv2d_cuda
+import torch
+import torch.nn as nn
+import math
+from torch.nn.common_types import _size_1_t, _size_2_t, _size_3_t
+from torch.nn.modules.utils import _single, _pair, _triple, _reverse_repeat_tuple
+
+from typing import Optional, List, Tuple, Union
+
+class Conv2DFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, weight, params):
+        stride = _pair(params[0])
+        padding = _pair(params[1])
+        output = conv2d_cuda.forward(input.contiguous(),weight.contiguous(),(stride),(padding))
+        variables = [input, weight, params]
+        ctx.save_for_backward(*variables)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, weight, params = ctx.saved_variables 
+        stride = _pair(params[0])
+        padding = _pair(params[1])
+        grad_input,grad_weight = conv2d_cuda.backward(input.contiguous(), grad_output.contiguous(), weight.contiguous(),(stride),(padding))
+        return grad_input, grad_weight, None
+    
+
+class Conv2d(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: _size_2_t,
+        stride: _size_2_t = 1,
+        padding: Union[str, _size_2_t] = 0,
+        bias: bool = False,
+        device=None,
+        dtype=None
+    ):
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        
+        kernel_size_ = _pair(kernel_size)
+        stride_ = _pair(stride)
+        padding_ = _pair(padding)
+        super(Conv2d, self).__init__()
+
+        self.params=torch.Tensor([stride_,padding_])
+        self.weight = nn.Parameter(torch.empty(out_channels, in_channels, *kernel_size_, **factory_kwargs))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+
+    def forward(self, input):
+        return Conv2DFunction.apply(input, self.weight, self.params)
